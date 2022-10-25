ecf0ee5793750ed32509479493f3fc64ab2ddcc4
slozhkai <sisipopakaka@gmail.com>
2022-10-25 17:44:50 +0300

    f

--- pz2/pz2_1.py
+++ pz2/pz2_1.py
@@ -1,14 +1,12 @@
 a=input('Введите число больше 999')
-while type(a)!=int():
+while type(a)!=int:
     try:
         a=int(a)
-        if a > 999:
-            print(a//100%10)
-            break
-        else:
-            print('Число должно быть больше 999')
-            continue
-        break
     except ValueError:
         print('неправильно ввели')
         a = input('Введите число больше 999')
+
+if a > 999:
+     print(a//100%10)
+else:
+        print('Число должно быть больше 999')
